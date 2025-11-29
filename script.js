6b54df093dcc818955cb1f7eda9c10a9a3e3b359
Bryan <bryanwagoki2016@gmail.com>
2025-11-29 20:08:58 +0300

    Add the Math.random method to create the logic to randomly return "rock", "paper" or "scissors"

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello World, this is Rock-Paper-Scissors!");
+
+function getComputerChoice() {
+    const randomVal = Math.random();
+    const choice = randomVal < 0.3333 ? "rock": randomVal < 0.6666 ? "paper" : "scissors";
+    return choice;
+}
+
+console.log(getComputerChoice());
